7b3def38850a1738ebd7ad858499c3a5b7992a6d
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-06-01 16:12:24 +0200

    ts-check Revision

---
+++ script.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * Calculates the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function calculateSum(a, b) {
+    return a + b;
+  }
+  
+  // Testing the function
+  const result = calculateSum(5, 10);
+  console.log(result);
+  
